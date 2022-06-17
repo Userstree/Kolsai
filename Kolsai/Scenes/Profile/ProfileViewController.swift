d996146dc539b340567f85a94b199b88c0af2bb6
Aigerim98 <aigerim.abdurahmanova@gmail.com>
2022-06-17 15:16:50 +0600

    before pod init

--- Kolsai/Scenes/Profile/ProfileViewController.swift
+++ Kolsai/Scenes/Profile/ProfileViewController.swift
@@ -5,9 +5,43 @@
 import UIKit
 
 class ProfileViewController: UIViewController {
+    
+//    private var nameLable: UILabel = {
+//        let label = UILabel()
+//        label.font = UIFont.systemFont(ofSize: 20, weight: .bold)
+//        label.textColor = .black
+//        label.translatesAutoresizingMaskIntoConstraints = false
+//        return label
+//    }()
+//
+//    private var profileImageView: UIImageView = {
+//        let image = UIImageView()
+//        image.contentMode = .scaleToFill
+//        image.translatesAutoresizingMaskIntoConstraints = false
+//        return image
+//    }
+    private let tableView = UITableView()
+    private var safeArea: UILayoutGuide!
+    private var headers: [String] = ["Настройки","Дополнительно"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         view.backgroundColor = .secondarySystemBackground
+        safeArea = view.layoutMarginsGuide
     }
 }
+
+extension ProfileViewController: UITableViewDelegate, UITableViewDataSource  {
+  
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        2
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! TableViewCell
+        return cell
+    }
+
+}
+
